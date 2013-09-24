12b2be936c13ecbb83d1e8196d9739ef11e02b00
Luis Cordova <cordoval@gmail.com>
2013-09-24 11:27:09 -0500

    wire bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Simulator\Domain\Infrastructure\Symfony\Bundle\SimulatorBundle\CordovalSimulatorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
